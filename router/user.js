ea4d990874150e89ef4d0fe03750584bd01998ee
Wang Pengyue <wangpengyue@liuyuandeMacBook-Pro.local>
2020-01-02 22:32:41 +0800

    add result

--- router/user.js
+++ router/user.js
@@ -1,13 +1,17 @@
 const express = require('express')
+const Result = require('../models/Result')
 
 const router = express.Router()
 
 router.post('/login', function(req, res, next) {
-  console.log('/user/login', req.body)
-  res.json({
-    code: 0,
-    msg: '登录成功'
-  })
+  const username = req.body.username
+  const password = req.body.password
+
+  if (username === 'admin' && password === '123456') {
+    new Result('登录成功').success(res)
+  } else {
+    new Result('登录失败').fail(res)
+  }
 })
 
 module.exports = router

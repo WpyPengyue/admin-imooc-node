28ab5bd2f0cd8df0e73e54293e2e1a0234051832
Wang Pengyue <wangpengyue@liuyuandeMacBook-Pro.local>
2020-01-05 22:54:11 +0800

    add login

--- router/user.js
+++ router/user.js
@@ -1,17 +1,27 @@
 const express = require('express')
 const Result = require('../models/Result')
-
+const { login } = require('../services/user')
+const { md5 } = require('../utils/index')
+const { PWD_SALT } = require('../utils/constant')
 const router = express.Router()
 
-router.post('/login', function(req, res, next) {
-  const username = req.body.username
-  const password = req.body.password
+router.post('/login', function (req, res, next) {
+  let { username, password } = req.body
+  password = md5(username + PWD_SALT)
 
-  if (username === 'admin' && password === '123456') {
-    new Result('登录成功').success(res)
-  } else {
-    new Result('登录失败').fail(res)
-  }
+  login(username, password).then(user => {
+    if (!user || user.length === 0) {
+      new Result('登录失败').fail(res)
+    } else {
+      new Result('登录成功').success(res)
+    }
+  })
+
+  // if (username === 'admin' && password === '123456') {
+  //   new Result('登录成功').success(res)
+  // } else {
+  //   new Result('登录失败').fail(res)
+  // }
 })
 
 module.exports = router

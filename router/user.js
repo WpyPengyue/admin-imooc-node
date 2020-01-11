ab718d1b20bea4b04a5ad5a77130fcea21719b50
Wang Pengyue <wangpengyue@liuyuandeMacBook-Pro.local>
2020-01-11 11:54:35 +0800

    add jwt token

--- router/user.js
+++ router/user.js
@@ -5,7 +5,8 @@ const { md5 } = require('../utils/index')
 const { PWD_SALT } = require('../utils/constant')
 const { body, validationResult } = require('express-validator')
 const boom = require('boom')
-
+const jwt = require('jsonwebtoken')
+const { PRIVATE_KEY, JWT_EXPIRED } = require('../utils/constant')
 const router = express.Router()
 
 router.post('/login',
@@ -26,7 +27,12 @@ router.post('/login',
         if (!user || user.length === 0) {
           new Result('登录失败').fail(res)
         } else {
-          new Result('登录成功').success(res)
+          const token = jwt.sign(
+            { username },
+            PRIVATE_KEY,
+            { expiresIn: JWT_EXPIRED }
+          )
+          new Result({token}, '登录成功').success(res)
         }
       })
     }

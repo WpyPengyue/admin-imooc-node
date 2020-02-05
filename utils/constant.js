297b95ced06030a10ce32aa55625f5562bb5abed
Wang Pengyue <wangpengyue@liuyuandeMacBook-Pro.local>
2020-02-05 16:07:39 +0800

    upload book logic

--- utils/constant.js
+++ utils/constant.js
@@ -1,3 +1,7 @@
+const {env} = require('./env')
+
+const UPLOAD_PATH = env === 'dev' ? '/Users/wangpengyue/Documents/upload/ebook':'';
+
 module.exports = {
     CODE_ERROR: -1,
     CODE_SUCCESS: 0,
@@ -6,4 +10,5 @@ module.exports = {
     PWD_SALT: 'admin_imooc_node',
     PRIVATE_KEY: 'wangpengyue_private_key',
     JWT_EXPIRED: 60 * 60, // token失效时间
+    UPLOAD_PATH: UPLOAD_PATH
 }
